8018c27200fa8f1ea2566a4996bdf5d473cbac0a
zxy <coordinate@live.cn>
2024-11-24 00:18:51 +0800

    ...

--- src/pages/home/previews/video_box.tsx
+++ src/pages/home/previews/video_box.tsx
@@ -19,41 +19,41 @@ Artplayer.PLAYBACK_RATE = [0.5, 0.75, 1, 1.25, 1.5, 2, 3, 4]
 
 // ******************************
 export const players: { icon: string; name: string; scheme: string }[] = [
-  { icon: "iina", name: "IINA", scheme: "iina://weblink?url=$durl" },
-  { icon: "potplayer", name: "PotPlayer", scheme: "potplayer://$durl" },
-  { icon: "vlc", name: "VLC", scheme: "vlc://$edurl" },
-  { icon: "nplayer", name: "nPlayer", scheme: "nplayer-$durl" },
+  { icon: "iina", name: "IINA", scheme: "iina://weblink?url=$eurl" },
+  { icon: "potplayer", name: "PotPlayer", scheme: "potplayer://$eurl" },
+  { icon: "vlc", name: "VLC", scheme: "vlc://$eurl" },
+  { icon: "nplayer", name: "nPlayer", scheme: "nplayer-$eurl" },
   {
     icon: "omniplayer",
     name: "OmniPlayer",
-    scheme: "omniplayer://weblink?url=$durl",
+    scheme: "omniplayer://weblink?url=$eurl",
   },
   {
     icon: "figplayer",
     name: "Fig Player",
-    scheme: "figplayer://weblink?url=$durl",
+    scheme: "figplayer://weblink?url=$eurl",
   },
   {
     icon: "infuse",
     name: "Infuse",
-    scheme: "infuse://x-callback-url/play?url=$durl",
+    scheme: "infuse://x-callback-url/play?url=$eurl",
   },
   {
     icon: "fileball",
     name: "Fileball",
-    scheme: "filebox://play?url=$durl",
+    scheme: "filebox://play?url=$eurl",
   },
   {
     icon: "mxplayer",
     name: "MX Player",
     scheme:
-      "intent:$edurl#Intent;package=com.mxtech.videoplayer.ad;S.title=$name;end",
+      "intent:$eurl#Intent;package=com.mxtech.videoplayer.ad;S.title=$name;end",
   },
   {
     icon: "mxplayer-pro",
     name: "MX Player Pro",
     scheme:
-      "intent:$durl#Intent;package=com.mxtech.videoplayer.pro;S.title=$name;end",
+      "intent:$eurl#Intent;package=com.mxtech.videoplayer.pro;S.title=$name;end",
   },
 ]
 // ******************************
@@ -147,18 +147,18 @@ export const VideoBox = (props: {
         </For>
       </Flex>
       <Anchor
-        href={convertURL("vlc://$edurl", {
+        href={convertURL("vlc://$eurl", {
           raw_url: objStore.raw_url,
           name: objStore.obj.name,
           d_url: currentObjLink(true),
         })}
         external
       >
-        {currentObjLink(true)}
+        {objStore.raw_url}
       </Anchor>
       <Anchor
         href={convertURL(
-          "intent:$edurl#Intent;package=com.mxtech.videoplayer.ad;S.title=$name;end",
+          "intent:$eurl#Intent;package=com.mxtech.videoplayer.ad;S.title=$name;end",
           {
             raw_url: objStore.raw_url,
             name: objStore.obj.name,
